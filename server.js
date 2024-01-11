8b99f389a387f694dbfcfc41c4b3bd258c230105
marcelohcortez <marcelohcortez@gmail.com>
2024-01-11 19:44:52 +0100

    Add cors 3rd party to fix CORS issues

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
@@ -17,8 +18,8 @@ app.use((req, res, next) => {
 })
 
 //routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/workouts', cors(), workoutRoutes)
+app.use('/api/user', cors(), userRoutes)
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI)
